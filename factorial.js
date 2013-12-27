4b7410096c80d37edcc3745743bf575328059abc
JFingert <joshuafingert@gmail.com>
2013-12-27 12:30:29 -0800

    factorial finder

---
+++ factorial.js
@@ -0,0 +1,14 @@
+function factorializer(num) { 
+
+	product = 1;
+
+	for (var i = 2; i <= num; i++){
+		product *= i;
+	}
+	return product;
+}
+
+
+var answer = prompt("Enter ye number to find its factorial");
+	
+factorializer(answer);
